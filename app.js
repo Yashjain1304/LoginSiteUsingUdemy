6d146ec7486ffc1ac26710b128c1cd5dfd704565
Yashjain1304 <yashjainyj1304200@gmail.com>
2021-05-11 23:26:00 +0530

    second level encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +14,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+const secret = "thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret,  encryptedFields: ['password'] });
 
 const UserModel = mongoose.model("userdb", userSchema);
 
